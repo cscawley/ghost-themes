2b073bd8e4dd714c173972630928228323a010db
cscawley <chris@ampelos.io>
2015-12-27 03:22:22 -0600

    created a big site-hero with morphing navi-header using waypoints js library. I also added author image to bottom of post.hbs

--- The-Shell-master/assets/js/shell.js
+++ The-Shell-master/assets/js/shell.js
@@ -2,3 +2,20 @@ $(function(){
 	$('pre').addClass('prettyprint');
 	prettyPrint();
 });
+$(function(){
+	var $head = $( '#navi-header' );
+	$( '.js-waypoint' ).each( function(i) {
+		var $el = $( this ),
+			animClassDown = $el.data( 'animateDown' ),
+			animClassUp = $el.data( 'animateUp' );
+
+		$el.waypoint( function( direction ) {
+			if( direction === 'down' && animClassDown ) {
+				$head.attr('class', 'navi-header ' + animClassDown);
+			}
+			else if( direction === 'up' && animClassUp ){
+				$head.attr('class', 'navi-header ' + animClassUp);
+			}
+		}, { offset: '50%' } );
+	} );
+});
